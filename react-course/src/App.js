3be526a2276d04ec97c18a490f545c6f44a7556d
nightknight <nightknightamir@gmail.com>
2024-03-14 13:05:09 +0330

    create components

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,13 +1,12 @@
+import Todo from "./Components/Todo";
+
 function App() {
   return (
     <div>
       <h1>My todo</h1>
-      <div className="card">
-        <h2>title</h2>
-        <div className="actions">
-          <bottom className='btn'>delete</bottom>
-        </div>
-      </div>
+     
+      <Todo />
+      <Todo />
     </div>
   );
 }
---
+++ react-course/src/Components/Todo.js
@@ -0,0 +1,13 @@
+
+
+
+function Todo() {
+  <div className="card">
+    <h2>title</h2>
+    <div className="actions">
+      <bottom className="btn">delete</bottom>
+    </div>
+  </div>;
+}
+
+export default Todo;
