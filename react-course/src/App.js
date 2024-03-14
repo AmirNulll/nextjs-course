ae781a3044ea7c584952e2d9e90cc5bbbcfe5e18
nightknight <nightknightamir@gmail.com>
2024-03-14 11:59:20 +0330

    attach style

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,6 +1,15 @@
 function App() {
-  return;
-  <div>Hello</div>;
+  return (
+    <div>
+      <h1>My todo</h1>
+      <div className="card">
+        <h2>title</h2>
+        <div className="actions">
+          <bottom className='btn'>delete</bottom>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
